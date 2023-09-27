cb3c37cdfcf9e659465cfb8beedac65e886c116f
tanish1409 <tanishsingla02@gmail.com>
2023-09-27 19:57:16 -0400

    Fix driver class to remove the file read. and update UML.

--- src/fruitfactory/AppleFactory.java
+++ src/fruitfactory/AppleFactory.java
@@ -1,13 +1,37 @@
 package fruitfactory;
 
+import java.util.Objects;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class AppleFactory implements GroceryProductFactory {
     String productName = "Apple";
+    String price = "0";
+    public AppleFactory() {
+        try {
+            File file = new File("src/database.txt");
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] split = line.split(" ");
+                String product = split[0];
+                if (Objects.equals(product, productName))
+                    price = split[1];
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public GroceryProduct makeProduct() {
         GroceryProduct product = new Apples();
-        product.setPrice(0);
+        product.setPrice(Float.parseFloat(price));
         System.out.println("A new " + productName + " has been created.");
         return product;
     }
+
 }
--- src/fruitfactory/BananaFactory.java
+++ src/fruitfactory/BananaFactory.java
@@ -1,12 +1,36 @@
 package fruitfactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class BananaFactory implements GroceryProductFactory {
     String productName = "Banana";
+    String price = "0";
+
+    public BananaFactory() {
+        try {
+            File file = new File("src/database.txt");
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] split = line.split(" ");
+                String product = split[0];
+                if (Objects.equals(product, productName))
+                    price = split[1];
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public GroceryProduct makeProduct() {
         GroceryProduct product = new Bananas();
-        product.setPrice(0);
+        product.setPrice(Float.parseFloat(price));
         System.out.println("A new " + productName + " has been created.");
         return product;
     }
